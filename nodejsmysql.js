cb42c7d2cfd150b7ae782e0c1adf220f421ea2da
veryverygood119@gmail.com <veryverygood119@gmail.com>
2025-12-03 18:24:01 +0800

    提交更新

--- nodejsmysql.js
+++ nodejsmysql.js
@@ -10,7 +10,27 @@ var connection = mysql.createConnection({
  
 connection.connect();
  
-var  sql = 'SELECT * FROM xinxi';
+// 插入数据
+var  addSql = 'INSERT INTO websites(Id,name,url,alexa,country) VALUES(0,?,?,?,?)';
+var  addSqlParams = ['菜鸟工具', 'https://c.runoob.com','23453', 'CN'];
+//增
+connection.query(addSql,addSqlParams,function (err, result) {
+        if(err){
+         console.log('[INSERT ERROR] - ',err.message);
+         return;
+        }        
+ 
+       console.log('--------------------------INSERT----------------------------');
+       //console.log('INSERT ID:',result.insertId);        
+       console.log('INSERT ID:',result);        
+       console.log('-----------------------------------------------------------------\n\n');  
+});
+
+
+
+
+// 查询数据
+var  sql = 'SELECT * FROM websites';
 //查
 connection.query(sql,function (err, result) {
         if(err){
@@ -22,5 +42,40 @@ connection.query(sql,function (err, result) {
        console.log(result);
        console.log('------------------------------------------------------------\n\n');  
 });
+
+
+
+
+
+// 删除数据
+var delSql = 'DELETE FROM websites where id=6';
+//删
+connection.query(delSql,function (err, result) {
+        if(err){
+          console.log('[DELETE ERROR] - ',err.message);
+          return;
+        }        
+ 
+       console.log('--------------------------DELETE----------------------------');
+       console.log('DELETE affectedRows',result.affectedRows);
+       console.log('-----------------------------------------------------------------\n\n');  
+});
+
+
+// 更新数据
+var modSql = 'UPDATE websites SET name = ?,url = ? WHERE Id = ?';
+var modSqlParams = ['菜鸟移动站', 'https://m.runoob.com',6];
+//改
+connection.query(modSql,modSqlParams,function (err, result) {
+   if(err){
+         console.log('[UPDATE ERROR] - ',err.message);
+         return;
+   }        
+  console.log('--------------------------UPDATE----------------------------');
+  console.log('UPDATE affectedRows',result.affectedRows);
+  console.log('-----------------------------------------------------------------\n\n');
+});
+
+
  
 connection.end();
